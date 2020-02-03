98bcc49c438b9a58636941244fd35f12a68a94eb
ktalant <uchkunfan@gmail.com>
2020-02-02 19:18:05 -0500

    updated

--- main.tf
+++ main.tf
@@ -2,22 +2,22 @@ provider "aws" {
   region = "us-east-1"
 }
 
-data "aws_ami" "example" {
-  most_recent      = true
-
+data "aws_ami" "centos" {
+owners      = ["679593333241"]
+most_recent = true
 
   filter {
-    name   = "name"
-    values = ["Centos 7-*"]
+      name   = "name"
+      values = ["CentOS Linux 7 x86_64 HVM EBS *"]
   }
 
   filter {
-    name   = "root-device-type"
-    values = ["ebs"]
+      name   = "architecture"
+      values = ["x86_64"]
   }
 
   filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
+      name   = "root-device-type"
+      values = ["ebs"]
   }
 }

eaa2d2c6dede67b80f68a4deb8c4ed602f3abb0b
ktalant <uchkunfan@gmail.com>
2020-02-04 22:14:37 -0500

    updated

--- main.tf
+++ main.tf
@@ -87,7 +87,7 @@ resource "aws_route_table" "private_rt" {
 }
 
 resource "aws_route" "private_nat_gateway_route" {
-  route_table_id = aws_route_table.private.private_rt.id
+  route_table_id = aws_route_table.private_rt.id
   destination_cidr_block = "0.0.0.0/0"
   depends_on = [aws_route_table.private_rt]
   nat_gateway_id = aws_nat_gateway.talant_ngw.id

884dd82d10c1047be02dbb128247c690bf0a85e0
ktalant <uchkunfan@gmail.com>
2020-02-04 20:44:01 -0500

    internet gateway association

--- main.tf
+++ main.tf
@@ -2,13 +2,25 @@ provider "aws" {
   region = "us-east-1"
 }
 
+data "aws_availability_zones" "available" {}
+
 resource "aws_vpc" "talant_vpc" {
   cidr_block            = var.vpc_cidr
   enable_dns_support    = true
   enable_dns_hostnames  = true
 
   tags = {
-    Name = "main"
+    Name = "talant-VPC"
   }
 }
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.talant_vpc.id
+
+  tags = {
+    Name = "talant-IGW"
+  }
+}
+
+
 

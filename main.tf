13681c9ab383671596e89651c4095a12100158f6
ktalant <uchkunfan@gmail.com>
2020-02-04 21:18:39 -0500

    updated

--- main.tf
+++ main.tf
@@ -4,6 +4,7 @@ provider "aws" {
 
 data "aws_availability_zones" "available" {}
 
+# VPC
 resource "aws_vpc" "talant_vpc" {
   cidr_block            = var.vpc_cidr
   enable_dns_support    = true
@@ -14,6 +15,7 @@ resource "aws_vpc" "talant_vpc" {
   }
 }
 
+# Internet Gateway
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.talant_vpc.id
 
@@ -22,6 +24,7 @@ resource "aws_internet_gateway" "igw" {
   }
 }
 
+# Public Route Table
 resource "aws_default_route_table" "public_rt" {
   default_route_table_id = aws_vpc.talant_vpc.default_route_table_id
 
@@ -35,14 +38,47 @@ resource "aws_default_route_table" "public_rt" {
   }
 }
 
+# Private Route Table
 resource "aws_route_table" "private_rt" {
   vpc_id = aws_vpc.talant_vpc.id
-
 
   tags = {
     Name = "talant-private-RouteTable"
   }
 }
+
+# Subnets
+resource "aws_subnet" "talant_public_subnet" {
+  count                   = 2
+  vpc_id                  = aws_vpc.talant_vpc.id
+  cidr_block              = var.public_cidrs[count.index]
+  map_public_ip_on_launch = true
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+
+  tags {
+    Name = "talant_public_subnet${count.index + 1}"
+  }
+}
+
+# Public Subnet association
+resource "aws_route_table_association" "talant_public_assoc" {
+  count          = aws_subnet.talant_public_subnet.count
+  subnet_id      = aws_subnet.talant_public_subnet.*.id[count.index]
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_subnet" "talant_private_subnet" {
+  count                   = 2
+  vpc_id                  = aws_vpc.talant_vpc.id
+  cidr_block              = var.private_cidrs[count.index]
+  map_public_ip_on_launch = true
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+
+  tags {
+    Name = "talant_private_subnet${count.index + 1}"
+  }
+}
+
 
 
 
--- variables.tf
+++ variables.tf
@@ -1,2 +1,5 @@
 variable "vpc_cidr" {}
+variable "public_cidrs" {}
+variable "private_cidrs" {}
+
 
